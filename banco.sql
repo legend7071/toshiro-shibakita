edd51e46a67fa527d4b3b5855368880c19f78a0f
legend7071 <thelegendgamepro@gmail.com>
2025-04-21 15:42:29 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,10 +1,10 @@
 CREATE TABLE dados (
-    AlunoID int,
+    AlunoID int PRIMARY KEY,
     Nome varchar(50),
     Sobrenome varchar(50),
     Endereco varchar(150),
     Cidade varchar(50),
     Host varchar(50),
-    Pergunta de Segurança? ('Qual o nome da sua mãe?'),
+    PerguntaSeguranca varchar(100),
     Resposta varchar(50)
 );
